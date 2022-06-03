25e4b34b0d9718823cdc4268a2b9f7ce969f9a51
Akihiro Nitta <akihiro@pytorchlightning.ai>
2022-06-03 19:25:19 -0400

    rm download_datasets.py

--- .github/scripts/download_datasets.py
+++
@@ -1,8 +0,0 @@
-from torchvision.datasets import MNIST, CIFAR10
-
-
-DATASET_PATH = "./data"
-MNIST(root=DATASET_PATH, train=True, download=True)
-MNIST(root=DATASET_PATH, train=False, download=True)
-CIFAR10(root=DATASET_PATH, train=True, download=True)
-CIFAR10(root=DATASET_PATH, train=False, download=True)

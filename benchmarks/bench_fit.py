ab4dc17a1f67b91daa6a463fa9c851dce7781e92
Akihiro Nitta <nitta@akihironitta.com>
2022-08-08 10:35:26 +0900

    Update benchmarks

--- benchmarks/bench_fit.py
+++ benchmarks/bench_fit.py
@@ -1,23 +1,18 @@
 import numpy as np
-from torch.utils.data import DataLoader
 
-from benchmarks.common_data import RandomDataset
 from benchmarks.common_models import (
-    BoringModel,
-    ParityModuleCIFAR,
-    ParityModuleMNIST,
-    ParityModuleRNN,
-    YetAnotherBoringModel,
+    BoringParityModule,
+    CIFARParityModule,
+    MNISTParityModule,
 )
 from benchmarks.common_utils import measure_loops
-from pytorch_lightning import Trainer
 
 
-class MNISTBenchmark:
+class BoringBenchmark:
     timeout = 10 * 60
 
     def setup_cache(self):
-        return measure_loops(ParityModuleMNIST, kind="PT Lightning", num_epochs=4)
+        return measure_loops(BoringParityModule, kind="PT Lightning", num_epochs=4)
 
     def track_time(self, output):
         return np.mean(output["durations"])
@@ -29,9 +24,11 @@ class MNISTBenchmark:
         return np.mean(output["losses"])
 
 
-class RNNBenchmark:
+class MNISTBenchmark:
+    timeout = 10 * 60
+
     def setup_cache(self):
-        return measure_loops(ParityModuleRNN, kind="PT Lightning", num_epochs=4)
+        return measure_loops(MNISTParityModule, kind="PT Lightning", num_epochs=4)
 
     def track_time(self, output):
         return np.mean(output["durations"])
@@ -47,7 +44,7 @@ class CIFARBenchmark:
     timeout = 10 * 60
 
     def setup_cache(self):
-        return measure_loops(ParityModuleCIFAR, kind="PT Lightning", num_epochs=4)
+        return measure_loops(CIFARParityModule, kind="PT Lightning", num_epochs=4)
 
     def track_time(self, output):
         return np.mean(output["durations"])
@@ -57,64 +54,3 @@ class CIFARBenchmark:
 
     def track_last_loss(self, output):
         return np.mean(output["losses"])
-
-
-class BoringBenchmark:
-    def setup_cache(self):
-        train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-        model = BoringModel()
-        trainer = Trainer(
-            enable_progress_bar=False,
-            enable_model_summary=False,
-            enable_checkpointing=False,
-            max_epochs=20,
-        )
-        return trainer, model, train_data
-
-    def time_it(self, args):
-        trainer, model, train_data = args
-        trainer.fit(model, train_data)
-
-    def peakmem_it(self, args):
-        trainer, model, train_data = args
-        trainer.fit(model, train_data)
-
-
-class AnotherBoringBenchmark:
-    def fit_model(self):
-        data = DataLoader(RandomDataset(32, 64), batch_size=2)
-        model = BoringModel()
-        trainer = Trainer(
-            accelerator=None,
-            max_epochs=1,
-            logger=False,
-            benchmark=True,
-        )
-        trainer.fit(model, data)
-        trainer.test(model, data)
-        trainer.validate(model, data)
-
-    def time_it(self):
-        self.fit_model()
-
-    def peakmem_it(self):
-        self.fit_model()
-
-
-class YetAnotherBoringBenchmark:
-    def fit_model(self):
-        model = YetAnotherBoringModel()
-        trainer = Trainer(
-            max_epochs=1,
-            logger=False,
-            benchmark=True,  # False in 1.5.x and True in 1.6.x by default.
-        )
-        trainer.fit(model)
-        trainer.test(model)
-        trainer.validate(model)
-
-    def time_it(self):
-        self.fit_model()
-
-    def peakmem_it(self):
-        self.fit_model()
